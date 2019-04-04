bca54ef16a0a5299d316eecec03301554481bbda
Matthew Trabold <mct4240@gmail.com>
2019-04-03 21:07:14 -0500

    Update bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,3 +2,76 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var consoleTable = require("console.table");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "9908Rock!",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected as id " + connection.threadID);
+  // printInventory();
+  purchaseItem();
+});
+
+function printInventory() {
+  connection.query("SELECT * FROM products", function(err, response) {
+    if (err) throw err;
+    console.table(response);
+  });
+}
+
+function purchaseItem() {
+  connection.query("SELECT * FROM products", function(err, response) {
+    if(err) throw err;
+    console.table(response);
+      inquirer
+        .prompt([
+          {
+            name: "item_id",
+            type: "input",
+            message: "What is the ID of the product you would like to buy?"
+          },
+          {
+            name: "stock_quantity",
+            type: "input",
+            message: "How much would you like to buy?"
+          }
+        ]).then(function(answer) {
+
+          // itemChoice accesses the row in the database the user would like a product form
+          var itemChoice = parseInt(answer.item_id);
+          // This stores the amount of the requested product currently in stock
+          var currentStockQuantity = response[itemChoice].stock_quantity;
+          // Shows how much the user would like to purchase
+          var requestedPurchaseAmount = answer.stock_quantity;
+
+          var newStockAmount;
+          if(currentStockQuantity < requestedPurchaseAmount) {
+            console.log("Insufficient Quantity Order Will Not Be Processed");
+          } else {
+            newStockAmount = parseInt(currentStockQuantity - requestedPurchaseAmount);
+            console.log(itemChoice);
+            console.log(newStockAmount);
+            connection.query("UPDATE products SET ? WHERE ?",
+              [
+                {
+                  stock_quantity: parseInt(newStockAmount)
+                },
+                {
+                  item_id: response.item_id
+                }
+              ],
+              function(error) {
+                if (error) throw error;
+                printInventory();
+              }
+            );
+          };
+      });
+  });
+}
